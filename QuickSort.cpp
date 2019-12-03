06092173bbe767d67d6ef199910496326a258eb6
brettregnierm <bretternestregnier@gmail.com>
2019-12-02 20:27:50 -0700

    Implemented almost all of the functions

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -1,47 +1,95 @@
 #include <iostream>
 #include <vector>
+#include <stack>
 #include <random>
 #include <chrono>
+#include <omp.h>
+#include <stdio.h>
 
-#define INSERT_THRESH 4
+#define INSERT_THRESH 39
 
 template <typename T>
 void SerialInsertionSort(std::vector<T> &vec, int l, int r);
+
+double StartSerialQuickSort(int iter, int size);
 template <typename T>
 void SerialQuickSort(std::vector<T> &vec, int l, int r);
 
+double StartStackQuickSort1_0(int iter, int size, int threads);
+template <typename T>
+void StackQuickSort1_0(std::vector<T> &vec, int l, int r, std::stack<std::pair<int, int>> &stack, int &busythreads, const int threads);
+
+double StartNestedOMPSort1_0(int iter, int size, int threads);
+template <typename T>
+void NestedOMPSort1_0(std::vector<T> &vec, int l, int r, int &busythreads, int &threads);
+
+double StartTaskQueueSort1_0(int iter, int size, int threads);
+template <typename T>
+void TaskQueueSort1_0(std::vector<T> &vec, int l, int r);
+
+template <typename T>
+bool Validate(std::vector<T> &vec);
+template <typename T>
+void PrintResults(std::vector<T> &vec);
+
 int main()
 {
+    std::cout << "threads available = " << omp_get_max_threads() << std::endl;
+
+    double t;
+    int s = 10000000;
+
+    // t = StartSerialQuickSort(1, s);
+    // std::cout << "Elapsed serial sort = " << t << std::endl;
+
+    // t = StartStackQuickSort1_0(1, s, 4);
+    // std::cout << "Elapsed stack sort 1.0 = " << t << std::endl;
+
+    t = StartNestedOMPSort1_0(1, s, 4);
+    std::cout << "Elapsed nested omp sort 1.0 = " << t << std::endl;
+}
+
+template <typename T>
+void SerialInsertionSort(std::vector<T> &vec, int l, int r)
+{
+    int i = l;
+    int j;
+    while (i <= r)
+    {
+        j = i;
+        while (j > 0 && vec[j - 1] > vec[j])
+        {
+            std::swap(vec[j], vec[j - 1]);
+            j = j - 1;
+        }
+        i = i + 1;
+    }
+}
+
+double StartSerialQuickSort(int iter, int size)
+{
+    double elapsed = 0.0f;
+    double start = 0.0f;
+    double stop = 0.0f;
     std::vector<int> vec;
-    int64_t elapsed = 0;
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < iter; i++)
     {
         vec.clear();
-        int n = 123456;
+        int n = size;
         for (int i = 0; i < n; i++)
-        {
-            vec.push_back(rand() % n + n);
-        }
+            vec.push_back(rand());
 
-        auto start = std::chrono::high_resolution_clock::now();
+        start = omp_get_wtime();
+
         SerialQuickSort(vec, 0, vec.size() - 1);
-        auto stop = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-        elapsed += duration.count();
+
+        stop = omp_get_wtime();
+        elapsed += stop - start;
     }
 
-    elapsed /= 10;
+    Validate(vec);
 
-    int leng = vec.size();
-    bool isSorted = true;
-    for (int i = 1; i < leng; i++)
-    {
-        if (vec[i - 1] > vec[i])
-            isSorted = false;
-    }
-
-    std::cout << "Sorted = " << isSorted << std::endl;
-    std::cout << "duration = " << elapsed << std::endl;
+    return elapsed / iter;
 }
 
 template <typename T>
@@ -51,7 +99,7 @@ void SerialQuickSort(std::vector<T> &vec, int l, int r)
     T tmp;
     int i, j;
 
-    if (r - l < INSERT_THRESH) // hard code for now
+    if (r - l < INSERT_THRESH)
     {
         SerialInsertionSort(vec, l, r);
         return;
@@ -83,21 +131,280 @@ void SerialQuickSort(std::vector<T> &vec, int l, int r)
     SerialQuickSort(vec, i + 1, r);
 }
 
-template <typename T>
-void SerialInsertionSort(std::vector<T> &vec, int l, int r)
+double StartStackQuickSort1_0(int iter, int size, int threads)
 {
-    int i = l;
-    int j;
-    while (i <= r)
+    double elapsed = 0.0f;
+    double start = 0.0f;
+    double stop = 0.0f;
+    std::vector<int> vec;
+    for (int i = 0; i < iter; i++)
     {
-        j = i;
-        while (j > 0 && vec[j - 1] > vec[j])
+        vec.clear();
+        int busythreads = 1;
+        std::stack<std::pair<int, int>> stack;
+
+        for (int i = 0; i < size; i++)
+            vec.push_back(rand());
+
+        start = omp_get_wtime();
+
+#pragma omp parallel num_threads(threads) shared(vec, stack, threads, busythreads)
         {
-            std::swap(vec[j], vec[j - 1]);
-            j = j - 1;
+            if (omp_get_thread_num() == 0)
+                StackQuickSort1_0(vec, 0, vec.size() - 1, stack, busythreads, threads);
+            else
+                StackQuickSort1_0(vec, 0, 0, stack, busythreads, threads);
         }
-        i = i + 1;
+
+        stop = omp_get_wtime();
+        elapsed += (stop - start);
     }
 
-    return;
+    Validate(vec);
+
+    return elapsed / iter;
+}
+
+template <typename T>
+void StackQuickSort1_0(std::vector<T> &vec, int l, int r, std::stack<std::pair<int, int>> &stack, int &busythreads, const int threads)
+{
+    T pivot;
+    T tmp;
+    int i, j;
+    bool idle = true;
+    std::pair<int, int> bound;
+
+    if (l != r)
+        idle = false;
+
+    while (true)
+    {
+        if (r - l < INSERT_THRESH)
+        {
+            SerialInsertionSort(vec, l, r);
+            l = r;
+        }
+
+        while (l >= r)
+        {
+#pragma omp critical
+            {
+                // if there is stuff on the stack
+                if (stack.empty() == false)
+                {
+                    if (idle)
+                        ++busythreads;
+                    idle = false;
+
+                    bound = stack.top();
+                    stack.pop();
+                    l = bound.first;
+                    r = bound.second;
+                }
+                else
+                {
+                    if (idle == false)
+                        --busythreads;
+                    idle = true;
+                }
+            }
+            // break out if the all threads are done
+            if (busythreads == 0)
+                return;
+        }
+
+        pivot = vec[r];
+        i = l - 1;
+        j = r;
+
+        while (true)
+        {
+            // std::cout << "here1" << std::endl;
+            while (vec[++i] < pivot)
+                ;
+            while (vec[--j] > pivot)
+                ;
+            if (i >= j)
+                break;
+            tmp = vec[i];
+            vec[i] = vec[j];
+            vec[j] = tmp;
+        }
+        tmp = vec[i];
+        vec[i] = vec[r];
+        vec[r] = tmp;
+
+        if (i - 1 - l > INSERT_THRESH)
+        {
+            bound = std::make_pair(l, i - 1);
+
+#pragma omp critical
+            {
+                stack.push(bound);
+            }
+        }
+        else
+            SerialInsertionSort(vec, l, i - 1);
+
+        l = i + 1;
+    }
+}
+
+double StartNestedOMPSort1_0(int iter, int size, int threads)
+{
+    double elapsed = 0.0f;
+    double start = 0.0f;
+    double stop = 0.0f;
+    std::vector<int> vec;
+    std::vector<double> timing;
+    for (int j = 10; j < 200; j++)
+    {
+            vec.clear();
+            int n = size;
+            for (int i = 0; i < n; i++)
+                vec.push_back(rand());
+
+            int busythreads = 1;
+
+            start = omp_get_wtime();
+
+            NestedOMPSort1_0(vec, 0, vec.size() - 1, busythreads, threads);
+
+            stop = omp_get_wtime();
+            elapsed += (stop - start);
+        timing.push_back(elapsed / iter);
+        elapsed = 0.0f;
+    }
+
+    Validate(vec);
+
+    return elapsed / iter;
+}
+
+template <typename T>
+void NestedOMPSort1_0(std::vector<T> &vec, int l, int r, int &busythreads, int &threads)
+{
+    T pivot;
+    T tmp;
+    int i, j;
+
+    if (r - l < INSERT_THRESH)
+    {
+        SerialInsertionSort(vec, l, r);
+        return;
+    }
+
+    pivot = vec[r];
+    i = l - 1;
+    j = r;
+
+    while (true)
+    {
+        // std::cout << "here1" << std::endl;
+        while (vec[++i] < pivot)
+            ;
+        while (vec[--j] > pivot)
+            ;
+        if (i >= j)
+            break;
+        tmp = vec[i];
+        vec[i] = vec[j];
+        vec[j] = tmp;
+    }
+    tmp = vec[i];
+    vec[i] = vec[r];
+    vec[r] = tmp;
+
+    if (busythreads >= threads)
+    {
+        NestedOMPSort1_0(vec, l, i - 1, busythreads, threads);
+        NestedOMPSort1_0(vec, i + 1, r, busythreads, threads);
+    }
+    else
+    {
+#pragma omp atomic update
+        busythreads += 2;
+
+#pragma omp parallel num_threads(threads) shared(vec, threads, busythreads, i, l, r)
+        {
+#pragma omp sections nowait
+            {
+#pragma omp section
+                {
+                    NestedOMPSort1_0(vec, l, i - 1, busythreads, threads);
+
+                    // this occurs because it will happen after the thread has come about out of the recursive step
+#pragma omp atomic
+                    busythreads--;
+                }
+#pragma omp section
+                {
+                    NestedOMPSort1_0(vec, i + 1, r, busythreads, threads);
+
+                    // this occurs because it will happen after the thread has come about out of the recursive step
+#pragma omp atomic
+                    busythreads--;
+                }
+            }
+        }
+    }
+}
+
+double StartTaskQueueSort1_0(int iter, int size, int threads)
+{
+}
+
+template <typename T>
+void TaskQueueSort1_0(std::vector<T> &vec, int l, int r)
+{
+
+    T pivot;
+    T tmp;
+    int i, j;
+
+    if (r - l < INSERT_THRESH)
+    {
+        SerialInsertionSort(vec, l, r);
+        return;
+    }
+
+    pivot = vec[r];
+    i = l - 1;
+    j = r;
+
+    while (true)
+    {
+        // std::cout << "here1" << std::endl;
+        while (vec[++i] < pivot)
+            ;
+        while (vec[--j] > pivot)
+            ;
+        if (i >= j)
+            break;
+        tmp = vec[i];
+        vec[i] = vec[j];
+        vec[j] = tmp;
+    }
+    tmp = vec[i];
+    vec[i] = vec[r];
+    vec[r] = tmp;
+
+#pragma omp tasq
+    {
+    }
+}
+
+template <typename T>
+bool Validate(std::vector<T> &vec)
+{
+    int leng = vec.size();
+    bool isSorted = true;
+    for (int i = 1; i < leng; i++)
+    {
+        if (vec[i - 1] > vec[i])
+            isSorted = false;
+    }
+
+    std::cout << "\nSorted = " << isSorted << std::endl;
+    return isSorted;
 }
